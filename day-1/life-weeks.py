588fb1115c6f5be6c5e3a5f68964b235b352e4d2
Sonith Kumar <sonith.kumar@gmail.com>
2022-12-03 22:47:08 +0530

    Coding test - Life in Weeks

---
+++ day-1/life-weeks.py
@@ -0,0 +1,11 @@
+age = input("What is your current age? ")
+
+x = (90 - int(age))*365
+y = (90 - int(age))*52
+z = (90 - int(age))*12
+
+print(f'You have {x} days, {y} weeks, and {z} months left.')
+
+
+
+
