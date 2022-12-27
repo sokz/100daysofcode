85528f29e7b9abdc4e4510093cf13f3572da63c6
Sonith Kumar <sonith.kumar@gmail.com>
2022-12-27 11:19:40 +0530

    README update

--- day-3/nestedif-else.py
+++ day-3/nestedif-else.py
@@ -8,9 +8,13 @@ height = int(input("What is your height in cm? "))
 if (height > 120):
     print("You can ride the rollercoaster!")
     age = int(input("What is your age? "))
-    if (age <= 18):
-        print("Please pay $7")
+    if (age < 12):
+        print("Child tickets are $5")
+    elif (age <= 18):
+        print("Youth tickets are $7")
+    elif(age >= 45) and (age <= 55):
+        print("Everything is going to be okay. Have a ride on us!")
     else:
-        print("Please pay $12")
+        print("Adult tickets are $12")
 else:
     print("Sorry, you have to grow taller before you can ride the rollercoaster.")
