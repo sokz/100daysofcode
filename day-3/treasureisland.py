67179d515663dae8f39810775b8d6b31eeb2e9a9
Sonith Kumar <sonith.kumar@gmail.com>
2023-01-02 00:25:16 +0530

    Some additional changes for treasureisland.py

--- day-3/treasureisland.py
+++ day-3/treasureisland.py
@@ -22,9 +22,21 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
-choice = input("left or right?").lower()
+
+choice = input("You're at a crossroad, where do you want to go? Type 'left' or 'right'.\n").lower()
 if choice == 'left':
-    print("Swim ")
-    progress = 'y'
+    choice = input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across. \n?").lower()
+    if choice == 'wait':
+        choice == input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower
+        if choice == 'red':
+            print("It's a room full of fire. Game Over.")
+        elif choice == 'blue':
+            print("You enter a room of beasts. Game Over.")
+        elif choice == 'yellow':
+            print("You found the treasure! You Win!")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+    else:
+        print("You get attacked by an angry trout. Game Over.")
 else:
-    
+    print("You fell into a hole. Game Over.")
