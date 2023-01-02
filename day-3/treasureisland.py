f8ee90113611da32915004e6a9cb9e137828acdb
Sonith Kumar <sonith.kumar@gmail.com>
2023-01-02 09:35:10 +0530

    Debugging treasureisland.py: removed the equality operator (==) from line 30 removed debugging print statements.

--- day-3/treasureisland.py
+++ day-3/treasureisland.py
@@ -25,9 +25,9 @@ print("Your mission is to find the treasure.")
 
 choice = input("You're at a crossroad, where do you want to go? Type 'left' or 'right'.\n").lower()
 if choice == 'left':
-    choice = input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across. \n?").lower()
+    choice = input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.?\n").lower()
     if choice == 'wait':
-        choice == input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower
+        choice = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
         if choice == 'red':
             print("It's a room full of fire. Game Over.")
         elif choice == 'blue':
